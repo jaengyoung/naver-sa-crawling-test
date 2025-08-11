3029dad1c52c030f2f060bd4745a60879a9457fc
jaengyoung <jaeyoung.lim@nhnad.com>
2025-08-11 11:00:10 +0900

    fix: 기존 인프라 삭제

--- infra/main.tf
+++ infra/main.tf
@@ -1,16 +1,16 @@
-module "ecr_naver_sa_crawler_test" {
-  source          = "git::https://github.com/NHNAD-wooyeon/hyper-infra-modules.git//terraform-aws-ecr"
-  repository_name = "naver-sa-crawler-test"
-}
+# module "ecr_naver_sa_crawler_test" {
+#   source          = "git::https://github.com/NHNAD-wooyeon/hyper-infra-modules.git//terraform-aws-ecr"
+#   repository_name = "naver-sa-crawler-test"
+# }
 
-module "lambda_naver_sa_crawler_test" {
-  source        = "git::https://github.com/NHNAD-wooyeon/hyper-infra-modules.git//terraform-aws-lambda"
-  function_name = "naver-sa-crawler-test"
-  package_type  = "Image"
-  image_uri     = "${module.ecr_naver_sa_crawler_test.repository_url}:latest"
-  timeout       = 60
-  memory_size   = 128
-  tracing_config = {
-    mode = "PassThrough"
-  }
-}
+# module "lambda_naver_sa_crawler_test" {
+#   source        = "git::https://github.com/NHNAD-wooyeon/hyper-infra-modules.git//terraform-aws-lambda"
+#   function_name = "naver-sa-crawler-test"
+#   package_type  = "Image"
+#   image_uri     = "${module.ecr_naver_sa_crawler_test.repository_url}:latest"
+#   timeout       = 60
+#   memory_size   = 128
+#   tracing_config = {
+#     mode = "PassThrough"
+#   }
+# }
